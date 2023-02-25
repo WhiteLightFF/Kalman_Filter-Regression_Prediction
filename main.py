0f4119a5d41ee66cc2803019a6c31b730d6e256c
WhiteLightFF <kirill.kokorev.2015@gmail.com>
2023-02-25 18:34:07 +0300

    Add regression predict

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ bbox = cv2.selectROI("Tracking", img, False)
 tracker.init(img, bbox)
 
 def onMouse(event, x,y, flags, param):
-    print('x = %d, y = %d' % (x, y))
+    #print('x = %d, y = %d' % (x, y))
+    pass
 def drawBox(img, bbox):
     x, y, w, h = int(bbox[0]), int(bbox[1]),int(bbox[2]),int(bbox[3])
     cv2.rectangle(img,(x,y),((x + w), (y+h)), (255,0,255), 3, 1)
     cv2.putText(img, "Tracking", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-    print(bbox)
+    #print(bbox)
 def ringBuffer(bbox):
     buf = np.zeros((10,2), dtype = int)
 
@@ -30,7 +31,7 @@ while True:
     timer = cv2.getTickCount()
     success, img = cap.read()
     success, bbox = tracker.update(img)
-    print(bbox)
+    #print(bbox)
     if success:
         drawBox(img, bbox)
     else:
@@ -44,18 +45,18 @@ while True:
         break
     centerX = int(bbox[0]+ bbox[2]/2)
     centerY = int(bbox[1]+ bbox[3]/2)
+    print(centerX, centerY)
 
     predicted = kf.predict(centerX, centerY)
 
-    cv2.circle(img, (centerX, centerY), 5, (255, 255, 255), 4)#центр цели
+    cv2.circle(img, (centerX, centerY), 5, (255, 255, 255), 4)#center object
 
 
     predicted = kf.predict(predicted[0], predicted[1])
-    cv2.circle(img, predicted, 5, (0, 255, 255), 4)  # старый калман
+    cv2.circle(img, predicted, 5, (0, 255, 255), 4)  # old kalman
 
     predictedNewCalman = kfnew.predict(centerX, centerY)
-    cv2.circle(img,  predictedNewCalman, 5, (255, 0, 0), 4) #новый калман
-
+    cv2.circle(img,  predictedNewCalman, 5, (255, 0, 0), 4) #new kalman
 
 
 
